806809210a442bd1e24c0792653aa2fbe0529bbe
Yohann Salaun <yohann.salaun@polytechnique.org>
2013-03-07 18:22:37 +0100

    before merging

--- teddy.bat
+++ teddy.bat
@@ -12,7 +12,7 @@ SET dmax=0
 SET dmap=%pathfile%\Images\%img%\disparity_map.png
 
 SET agreg_on=1
-SET so_on=1
+SET so_on=0
 
 SET cmd=%execfile% %img1% %img2% %dmin% %dmax% %dmap% %agreg_on% %so_on%
 
